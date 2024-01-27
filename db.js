670e6022ba69d89c6214671cae0d7e3aac6d8098
helyz007 <helyz.akande@gmail.com>
2024-01-27 02:40:45 +0100

    Fix: .env updated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 const DB = process.env.DATABASE;
 
 export const db = mongoose.connect(DB, {
